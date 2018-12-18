06f0c8e1e186d2008ff0a8f5959464404c1b8b91
akhil <akhil@grepruby.com>
2018-12-18 14:50:10 +0530

    added transaction history url for bitshares

---
+++ src/requestProcessor/getTransactionHistory.js
@@ -0,0 +1,24 @@
+import { getRequest, postRequest } from "../lib/request";
+import prepareBody from "../utility/requestBody"
+import { Apis } from "bitsharesjs-ws";
+import { ChainStore, FetchChain, TransactionBuilder } from "bitsharesjs";
+import _ from "lodash";
+import envConfig from "../../config/envConfig";
+
+
+const baseUrl = envConfig.get("baseUrl"); // get websocket url from config file.
+
+const processGetTransactionHistory = accountName =>
+new Promise((resolve, reject) => {
+  return Apis.instance("ws://192.168.10.81:11011", true) // TODO: Replace URL from a value from config file
+    .init_promise.then(res => {
+      return ChainStore.init();
+    })
+    .then(() => Promise.all([FetchChain("fetchFullAccount", "1.2.17")]))
+    .then(res => {
+      console.log("res: ", res)
+      resolve(res);
+    })
+    .catch(reject);
+})
+export default processGetTransactionHistory;
--- src/requestProcessor/index.js
+++ src/requestProcessor/index.js
@@ -1,5 +1,6 @@
 import processGetBalance from "./getBalanceProcessor";
 import processTransfer from "./transferBTSProcessor";
 import processCreateAccount from "./createAccountProcessor";
+import processGetTransactionHistory from "./getTransactionHistory"
 
-export default { processGetBalance, processTransfer, processCreateAccount };
+export default { processGetBalance, processTransfer, processCreateAccount, processGetTransactionHistory };
