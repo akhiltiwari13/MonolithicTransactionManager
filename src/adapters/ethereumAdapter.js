96d26d1d875eb1dc2d3f4499c39b8f299c696eb8
akhil <akhil@grepruby.com>
2018-12-20 13:34:02 +0530

    created a skeleton for ethAdapter modeled on bitsharesAdapter

--- src/adapters/ethereumAdapter.js
+++ src/adapters/ethereumAdapter.js
@@ -1,126 +1,193 @@
-import { postRequest, getRequest } from "../lib/request";
+import { postRequest } from "../lib/request";
+import prepareBody from "../utils/requestBody";
 import envConfig from "../../config/envConfig";
+import { Apis } from "bitsharesjs-ws";
+import { ChainStore, FetchChain, TransactionBuilder } from "bitsharesjs";
 import { getConnection } from "typeorm";
 import { User } from "../entity/user";
 import { Transfer } from "../entity/transfer";
 
-
-
-/* required constants for ethereum adapter */
-// const ETHEREUM_NODE_URL = `${config.get('ethereum_node_url.protocol')}${config.get('ethereum_node_url.host')}` ==> can this be used
-// this.web3 = new Web3(new Web3.providers.HttpProvider(ETHEREUM_NODE_URL))
-
+const BTSBaseUrl = envConfig.get("btsBaseUrl");  //what should be BTSBase URL?
 const vaultBaseUrl = envConfig.get("vaultBaseUrl");
-const Web3 = require('web3') // to get web3 to work.
-const priceBaseUrl = envConfig.get("priceBaseUrl");
-const ethChainId = 3; // 1 for mainnet and 3 for testnet
-
-
-/* ETH Transaction */
-if (typeof web3 !== 'undefined') {
-    console.log('Web3 found');
-    var web3 = new Web3(web3.currentProvider);
-} else {
-    // provider needs to be set from a config file.
-    var web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/90bfb15e855e41a8997b24d5f7a170e1"));
-}
 
 class EthereumAdapter {
     constructor(name) {
         this.name = name;
     }
 
-    // account creation for ethereum only requires the account be created on the Vault
+    getBalance = accountName =>
+        new Promise((resolve, reject) =>
+            this._getAccountId(`hwd${accountName}`)
+                .then(accountId => this._getAccountBalance(accountId))
+                .then(balance => resolve({ accountName, balance: balance / 100000, unit: "BTS" }))
+                .catch(reject));
+
+
+    getTransactionHistory = accountName =>
+        new Promise((resolve, reject) => {
+            return Apis.instance("ws://192.168.10.81:11011", true) // TODO: Replace URL from a value from config file
+                .init_promise.then(() => ChainStore.init())
+                .then(() => this._getAccountId(`hwd${accountName}`))
+                .then(accountId => Promise.all([FetchChain("fetchFullAccount", accountId)]))
+                .then(res => {
+                    let [fullAccountHistory] = res;
+                    resolve(fullAccountHistory.get("history"));
+                })
+                .catch(reject);
+        });
+
     createAccount = req =>
         new Promise((resolve, reject) => {
-            let userUuidVault;
+            let userUuidVault, publicKey, chainId;
             return this._registerUserToVault(req)
                 .then(result => {
                     userUuidVault = result.data.uuid;
-                    return userUuidVault;
+                    return this._getPublicKey(req, userUuidVault);
                 })
-                .then(res => {
-                    const connection = getConnection();
-                    const user = new User();
+                .then(pubKey => {
+                    let tr = new TransactionBuilder();
+                    let registrarAccount = "nathan";
                     let accountName = req.body.name;
-                    user.name = accountName;
-                    user.vault_uuid = userUuidVault;
-                    user.bts_publickey = "";
-                    connection.manager
-                        .save(user)
-                        .then(() => resolve(res[0].id))
-                        .catch(reject);
-                    return resolve({ uuid: userUuidVault, name: user.name });
-                })
-                .catch(reject);
-        });
+                    publicKey = pubKey;
+                    Apis.instance("ws://192.168.10.81:11011", true) // TODO: Replace URL
+                        .init_promise.then(res => {
+                            chainId = res[0].network.chain_id;
+                            return ChainStore.init();
+                        })
+                        .then(() => Promise.all([FetchChain("getAccount", registrarAccount)]))
+                        .then(res => {
+                            let [registrarAccount] = res;
 
-    // developed and working; need to test.
-    getBalance = (headers, accountName) =>
-        new Promise(async (resolve, reject) => {
-            const uuid = await this._getUuid(accountName);
-            return this._getAddress(headers, uuid)
-                .then(res => {
-                    return res;
-                })
-                .then(res => {
-                    return resolve(web3.eth.getBalance(res));
-                })
-                .catch(reject)
-        })
+                            /* Account Create */
+                            tr.add_type_operation("account_create", {
+                                referrer_percent: 0,
+                                registrar: registrarAccount.get("id"),
+                                referrer: registrarAccount.get("id"),
+                                name: `hwd${accountName}`,
+                                owner: {
+                                    weight_threshold: 1,
+                                    account_auths: [],
+                                    key_auths: [[publicKey, 1]],
+                                    address_auths: []
+                                },
+                                active: {
+                                    weight_threshold: 1,
+                                    account_auths: [],
+                                    key_auths: [[publicKey, 1]],
+                                    address_auths: []
+                                },
+                                options: {
+                                    memo_key: publicKey,
+                                    voting_account: "1.2.5",
+                                    num_witness: 0,
+                                    num_committee: 0,
+                                    votes: [],
+                                    extensions: []
+                                }
+                            });
+                            /* Account Create */
 
-    getPrice = (query) =>
-        new Promise((resolve, reject) => {
-            const url = `${priceBaseUrl}/data/price?fsym=ETH&tsyms=${query.currency}`;
-            const headers = { Apikey: 'f212d4142590ea9d2850d73ab9bb78b6f414da4613786c6a83b7e764e7bf67f7' };
-            return getRequest(url, {}, headers)
-                .then(result => resolve({ coin: 'ETH', [query.currency]: result[query.currency] }))
-                .catch(reject);
-        });
-
-
-    transfer = req =>
-        new Promise(async (resolve, reject) => {
-            const fromAccountUUID = await this._getUuid(req.body.fromAccount);
-            const toAccountUUID = await this._getUuid(req.body.toAccount);
-            const fromAccountAddress = await this._getAddress(req.headers, fromAccountUUID);
-            const toAccountAddress = await this._getAddress(req.headers, toAccountUUID);
-            const gasPrice = await this._getGasPrice();
-            const gasLimit = await this._getGas("", fromAccountAddress, toAccountAddress);
-            const amount = await this._getWeiFromEth(req.body.sendAmount);
-            const nounce = await this._getNounce(fromAccountAddress);
-
-            const payload = {
-                nonce: nounce,
-                value: Number(amount),
-                gasLimit: gasLimit,
-                gasPrice: gasPrice,
-                to: toAccountAddress,
-                data: "", // what will this take?
-                chainId: ethChainId
-            }
-            return this._signTransaction(payload, fromAccountUUID)
-                .then(signedTransaction => {
-                    console.log("signedTransaction: ", signedTransaction)
-                    const transactionHash = web3.eth.sendRawTransaction(signedTransaction.signature);
-                    return transactionHash;
-                })
-                .then(res => {
-                    const connection = getConnection();
-                    const transfer = new Transfer();
-                    transfer.txn_id = res;
-                    transfer.from = req.body.fromAccount;
-                    transfer.to = req.body.toAccount;
-                    transfer.amount = req.body.sendAmount;
-                    transfer.coin_id = 'ETH';
-                    transfer.txn_status = 'PENDING';
-                    connection.manager
-                        .save(transfer)
-                        .then(() => resolve(res))
+                            tr.set_required_fees();
+                        })
+                        .then(() => tr.finalize())
+                        .then(() => {
+                            const tr_buff = Buffer.concat([
+                                new Buffer(chainId, "hex"),
+                                tr.tr_buffer
+                            ]);
+                            const trBuff = tr_buff.toString("hex");
+                            return this._getSignature(trBuff, registrarAccount, true);
+                        })
+                        .then(sign => {
+                            tr.signatures.push(sign);
+                            return tr.broadcast();
+                        })
+                        .then(res => {
+                            const connection = getConnection();
+                            const user = new User();
+                            user.name = accountName;
+                            user.vault_uuid = userUuidVault;
+                            user.bts_publickey = publicKey;
+                            connection.manager
+                                .save(user)
+                                .then(() => resolve(res[0].id))
+                                .catch(reject);
+                        })
                         .catch(reject);
                 })
                 .catch(reject);
         });
+
+    // transfer = req =>
+    //     new Promise((resolve, reject) => {
+    //         let chainId;
+    //         const fromAccount = req.body.fromAccount;
+    //         const toAccount = req.body.toAccount;
+    //         const amount = req.body.sendAmount * 100000;
+    //         const sendAmount = {
+    //             amount,
+    //             asset: "BTS"
+    //         };
+    //         let tr = new TransactionBuilder();
+    //         Apis.instance("ws://192.168.10.81:11011", true)
+    //             .init_promise.then(res => {
+    //                 chainId = res[0].network.chain_id;
+    //                 return ChainStore.init();
+    //             })
+    //             .then(() =>
+    //                 Promise.all([
+    //                     FetchChain("getAccount", `hwd${fromAccount}`),
+    //                     FetchChain("getAccount", `hwd${toAccount}`),
+    //                     FetchChain("getAsset", sendAmount.asset),
+    //                     FetchChain("getAsset", sendAmount.asset)
+    //                 ])
+    //             )
+    //             .then(res => {
+    //                 let [fromAccount, toAccount, sendAsset, feeAsset] = res;
+
+    //                 tr.add_type_operation("transfer", {
+    //                     fee: {
+    //                         amount: 0,
+    //                         asset_id: feeAsset.get("id")
+    //                     },
+    //                     from: fromAccount.get("id"),
+    //                     to: toAccount.get("id"),
+    //                     amount: {
+    //                         amount: sendAmount.amount,
+    //                         asset_id: sendAsset.get("id")
+    //                     }
+    //                 });
+
+    //                 return tr.set_required_fees();
+    //             })
+    //             .then(() => tr.finalize())
+    //             .then(() => {
+    //                 const tr_buff = Buffer.concat([
+    //                     new Buffer(chainId, "hex"),
+    //                     tr.tr_buffer
+    //                 ]);
+    //                 const trBuff = tr_buff.toString("hex");
+    //                 return this._getSignature(trBuff, req.body.fromAccount, false);
+    //             })
+    //             .then(sign => {
+    //                 tr.signatures.push(sign);
+    //                 return tr.broadcast();
+    //             })
+    //             .then(res => {
+    //                 const connection = getConnection();
+    //                 const transfer = new Transfer();
+    //                 transfer.tx_id = res[0].id;
+    //                 transfer.from = fromAccount;
+    //                 transfer.to = toAccount;
+    //                 transfer.amount = amount;
+    //                 connection.manager
+    //                     .save(transfer)
+    //                     .then(() => resolve(res[0].id))
+    //                     .catch(reject);
+    //             })
+    //             .catch(reject);
+    //     });
+
     _registerUserToVault = req =>
         new Promise((resolve, reject) => {
             const url = `${vaultBaseUrl}/api/register`;
@@ -130,89 +197,88 @@ class EthereumAdapter {
                 .catch(reject);
         });
 
-    // utility function to get the address for the account. 
-    _getAddress = (headers, uuid) =>
+    _getPublicKey = (req, uuid) =>
         new Promise((resolve, reject) => {
             const body = {
-                coinType: 60,
-                path: "m/44'/60'/0'/0/0",
-                uuid: uuid
+                coinType: 240,
+                path: "",
+                uuid
             };
             const url = `${vaultBaseUrl}/api/address`;
-            const vaultHeaders = { "x-vault-token": headers["x-vault-token"] };
-            return postRequest(url, body, vaultHeaders)
-                .then(res => {
-                    return resolve(res.data.address);
-                })
+            const headers = { "x-vault-token": req.headers["x-vault-token"] };
+            return postRequest(url, body, headers)
+                .then(res => resolve(res.data.publicKey))
                 .catch(reject);
         });
 
-    _getUuid = async (account) => {
+    _getUuid = async (registrarAccount) => {
         const connection = getConnection();
         const UserRepository = connection.getRepository(User);
-        const registrar = await UserRepository.findOne({ name: account });
+        const registrar = await UserRepository.findOne({ name: registrarAccount });
         return registrar.vault_uuid;
     }
 
-    _getGas = async (data, from, to) => {
-        const gas = await web3.eth.estimateGas({ data, from, to });
-        return gas;
-    }
+    _getSignature = (trHex, registrarAccount, isRegister) =>
+        // new Promise(async (resolve, reject) => {
+        //     const url = `${vaultBaseUrl}/api/signature`;
+        //     const txDigest = {
+        //         transactionDigest: trHex
+        //     };
+        //     const registrarUuid = isRegister ? 'bgd3f7lgouhs7rjiapd0' : await this._getUuid(registrarAccount);
+        //     const body = {
+        //         coinType: 240,
+        //         path: "",
+        //         payload: JSON.stringify(txDigest),
+        //         uuid: registrarUuid
+        //     };
+        //     const headers = {
+        //         "x-vault-token": "5oPMP8ATL719MCtwZ1xN0r5s",
+        //         "Content-Type": "application/json"
+        //     };
+        //     return postRequest(url, body, headers)
+        //         .then(res => {
+        //             return resolve(res.data.signature);
+        //         })
+        //         .catch(reject);
+        // });
 
-    _getGasPrice = async () => {
-        const gasPrice = await (web3.eth.gasPrice * 1.5);
-        return gasPrice;
-    }
+    _getAccountId = accountName =>
+        new Promise((resolve, reject) => {
+            const body = prepareBody([0, "lookup_account_names", [[accountName]]]);
+            const url = `${BTSBaseUrl}/account_id?account_name=${accountName}`;
+            //   return postRequest(url, body)
+            //     .then(response => {
+            //       const accountId = response.result[0].id;
+            //       resolve(accountId);
+            //     })
+            return postRequest(url, body)
+                .then(response => {
+                    console.log("ETH: _getAccountId")
+                    resolve(true);
+                })
+                .catch(reject)
+        });
 
-    _getNounce = async (fromAccount) => {
-        const nounce = await web3.eth.getTransactionCount(fromAccount);
-        return nounce;
-    }
-
-    _getWeiFromEth = async (amount) => {
-        return web3.toWei(amount, 'ether');
-    }
-
-    _signTransaction = (payload, fromAccountUUID) => //LOG HERE....
-        new Promise(async (resolve, reject) => {
-            const url = `${vaultBaseUrl}/api/signature`;
-            const body = {
-                "coinType": 60,
-                "path": "m/44'/60'/0'/0/0",
-                "payload": JSON.stringify(payload),
-                "uuid": fromAccountUUID
-            }
-            console.log("body: ", body);
-            // needs to be passed as parameters instead...
-            const headers = {
-                "x-vault-token": "5oPMP8ATL719MCtwZ1xN0r5s",
-                "Content-Type": "application/json"
-            };
-            return postRequest(url, body, headers)
-                .then(res => {
-                    console.log("res:", res);
-                    return resolve(res.data);
+    _getAccountBalance = accountId =>
+        new Promise((resolve, reject) => {
+            const body = prepareBody([
+                0,
+                "get_account_balances",
+                [accountId, ["1.3.0"]]
+            ]); //hardcoded to get the BTS balance
+            const url = `${BTSBaseUrl}/rpc`;
+            //   return postRequest(url, body)
+            //     .then(response => {
+            //       const balance = response.result[0].amount;
+            //       resolve(balance);
+            //     })
+            return postRequest(url, body)
+                .then(response => {
+                    console.log("ETH: _getAccountBalance")
+                    resolve(true);
                 })
                 .catch(reject);
         });
-
-
-
-
-    // TO BE WORKED ON.....
-    getTransactionHistory = accountName =>
-        new Promise(async (resolve, reject) => {
-            const uuid = await this._getUuid(accountName);
-            return this._getAddress(headers, uuid)
-                .then(res => {
-                    console.log("address,", res) // returns address; needs business logic to actually fetch the balance.
-                    return res;
-                })
-                .then(res => {
-                    return resolve(web3.eth.getBalance(res)); // business logic for transaction history
-                })
-                .catch(reject)
-        })
 
 }
 
