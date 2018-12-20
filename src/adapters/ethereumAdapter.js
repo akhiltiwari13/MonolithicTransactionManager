ee9a8e82ef326f20f32a3f618753e417a413dbc9
akhil <akhil@grepruby.com>
2018-12-20 15:20:29 +0530

    added account creation logic for ethereum

--- src/adapters/ethereumAdapter.js
+++ src/adapters/ethereumAdapter.js
@@ -16,11 +16,17 @@ class EthereumAdapter {
     }
 
     getBalance = accountName =>
-        new Promise((resolve, reject) =>
-            this._getAccountId(`hwd${accountName}`)
-                .then(accountId => this._getAccountBalance(accountId))
-                .then(balance => resolve({ accountName, balance: balance / 100000, unit: "BTS" }))
-                .catch(reject));
+        new Promise((resolve, reject) => {
+            return this._getAccountId(accountName)
+                .then(res => {
+                    console.log()
+                })
+            // return this._getAccountId(`hwd${accountName}`)
+            //     .then(console.log)
+            //     // .then(accountId => this._getAccountBalance(accountId))
+            //     // .then(balance => resolve({ accountName, balance: balance / 100000, unit: "BTS" }))
+            //     .catch(reject)
+        })
 
 
     getTransactionHistory = accountName =>
@@ -36,85 +42,28 @@ class EthereumAdapter {
                 .catch(reject);
         });
 
+    // account creation for ethereum only requires the account be created on the Vault
     createAccount = req =>
         new Promise((resolve, reject) => {
-            let userUuidVault, publicKey, chainId;
+            let userUuidVault;
             return this._registerUserToVault(req)
                 .then(result => {
                     userUuidVault = result.data.uuid;
-                    return this._getPublicKey(req, userUuidVault);
+                    return userUuidVault;
                 })
-                .then(pubKey => {
-                    let tr = new TransactionBuilder();
-                    let registrarAccount = "nathan";
+                .then(res => {
+                    const connection = getConnection();
+                    const user = new User();
                     let accountName = req.body.name;
-                    publicKey = pubKey;
-                    Apis.instance("ws://192.168.10.81:11011", true) // TODO: Replace URL
-                        .init_promise.then(res => {
-                            chainId = res[0].network.chain_id;
-                            return ChainStore.init();
-                        })
-                        .then(() => Promise.all([FetchChain("getAccount", registrarAccount)]))
-                        .then(res => {
-                            let [registrarAccount] = res;
-
-                            /* Account Create */
-                            tr.add_type_operation("account_create", {
-                                referrer_percent: 0,
-                                registrar: registrarAccount.get("id"),
-                                referrer: registrarAccount.get("id"),
-                                name: `hwd${accountName}`,
-                                owner: {
-                                    weight_threshold: 1,
-                                    account_auths: [],
-                                    key_auths: [[publicKey, 1]],
-                                    address_auths: []
-                                },
-                                active: {
-                                    weight_threshold: 1,
-                                    account_auths: [],
-                                    key_auths: [[publicKey, 1]],
-                                    address_auths: []
-                                },
-                                options: {
-                                    memo_key: publicKey,
-                                    voting_account: "1.2.5",
-                                    num_witness: 0,
-                                    num_committee: 0,
-                                    votes: [],
-                                    extensions: []
-                                }
-                            });
-                            /* Account Create */
-
-                            tr.set_required_fees();
-                        })
-                        .then(() => tr.finalize())
-                        .then(() => {
-                            const tr_buff = Buffer.concat([
-                                new Buffer(chainId, "hex"),
-                                tr.tr_buffer
-                            ]);
-                            const trBuff = tr_buff.toString("hex");
-                            return this._getSignature(trBuff, registrarAccount, true);
-                        })
-                        .then(sign => {
-                            tr.signatures.push(sign);
-                            return tr.broadcast();
-                        })
-                        .then(res => {
-                            const connection = getConnection();
-                            const user = new User();
-                            user.name = accountName;
-                            user.vault_uuid = userUuidVault;
-                            user.bts_publickey = publicKey;
-                            connection.manager
-                                .save(user)
-                                .then(() => resolve(res[0].id))
-                                .catch(reject);
-                        })
-                        .catch(reject);
-                })
+                    user.name = accountName;
+                    user.vault_uuid = userUuidVault;
+                    user.bts_publickey = "";
+                    connection.manager
+                      .save(user)
+                      .then(() => resolve(res[0].id))
+                      .catch(reject);
+                      return resolve({ uuid: userUuidVault, name:  user.name });
+                  })
                 .catch(reject);
         });
 
@@ -218,29 +167,29 @@ class EthereumAdapter {
         return registrar.vault_uuid;
     }
 
-    _getSignature = (trHex, registrarAccount, isRegister) =>
-        // new Promise(async (resolve, reject) => {
-        //     const url = `${vaultBaseUrl}/api/signature`;
-        //     const txDigest = {
-        //         transactionDigest: trHex
-        //     };
-        //     const registrarUuid = isRegister ? 'bgd3f7lgouhs7rjiapd0' : await this._getUuid(registrarAccount);
-        //     const body = {
-        //         coinType: 240,
-        //         path: "",
-        //         payload: JSON.stringify(txDigest),
-        //         uuid: registrarUuid
-        //     };
-        //     const headers = {
-        //         "x-vault-token": "5oPMP8ATL719MCtwZ1xN0r5s",
-        //         "Content-Type": "application/json"
-        //     };
-        //     return postRequest(url, body, headers)
-        //         .then(res => {
-        //             return resolve(res.data.signature);
-        //         })
-        //         .catch(reject);
-        // });
+    _getSignature = (trHex, registrarAccount, isRegister) => { }
+    // new Promise(async (resolve, reject) => {
+    //     const url = `${vaultBaseUrl}/api/signature`;
+    //     const txDigest = {
+    //         transactionDigest: trHex
+    //     };
+    //     const registrarUuid = isRegister ? 'bgd3f7lgouhs7rjiapd0' : await this._getUuid(registrarAccount);
+    //     const body = {
+    //         coinType: 240,
+    //         path: "",
+    //         payload: JSON.stringify(txDigest),
+    //         uuid: registrarUuid
+    //     };
+    //     const headers = {
+    //         "x-vault-token": "5oPMP8ATL719MCtwZ1xN0r5s",
+    //         "Content-Type": "application/json"
+    //     };
+    //     return postRequest(url, body, headers)
+    //         .then(res => {
+    //             return resolve(res.data.signature);
+    //         })
+    //         .catch(reject);
+    // });
 
     _getAccountId = accountName =>
         new Promise((resolve, reject) => {
