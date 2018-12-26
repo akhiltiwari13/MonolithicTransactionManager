1e45dc05f94b0d38c1758616ad1f66cfe126ad16
Ankit Gupta <agupta@grepruby.com>
2018-12-26 15:48:17 +0530

    Replaces: userController to accountController

--- src/controller/accountController.js
+++ src/controller/accountController.js
@@ -1,26 +1,24 @@
 import Responder from '../lib/expressResponder';
+import * as Adapters from '../adapters';
 
-export default class UserController {
-  static getBalance = (req, res) => {
-    const accountName = req.params.userId;
-    return req.adapter
-      .getBalance(accountName)
+export default class AccountController {
+  static getBalance = (req, res) =>
+    req.adapter
+      .getBalance(req.headers, req.params.accountName)
       .then(balanceObject => Responder.success(res, balanceObject))
       .catch(error => Responder.operationFailed(res, error));
-  };
 
-  static getTransactionHistory = (req, res) => {
-    const accountName = req.params.userId;
-    return req.adapter
-      .getTransactionHistory(accountName)
+  static getTxnHistory = (req, res) =>
+    req.adapter
+      .getTransactionHistory(req.headers, req.params.accountName)
       .then(txnObject => Responder.success(res, txnObject))
       .catch(error => Responder.operationFailed(res, error));
-  };
 
-  static registerUser = (req, res) => {
+  static createAccount = (req, res) =>{
+    req.adapter = Adapters.getBlockchain('BTS');
     return req.adapter
-      .registerUser(req)
+      .createAccount(req)
       .then(result => Responder.success(res, result))
       .catch(error => Responder.operationFailed(res, error));
-  };
-}
+  }
+};
--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,6 @@
-import UserController from './userController';
+import AccountController from './accountController';
 import TransferController from './transferController';
 import HealthController from './healthController';
 import CoinController from './coinController';
 
-export { UserController, TransferController, HealthController, CoinController };
+export { AccountController, TransferController, HealthController, CoinController };
